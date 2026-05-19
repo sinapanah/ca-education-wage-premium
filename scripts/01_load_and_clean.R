becff31defce457ad53f009c31feadc3bc27b0e6
Sina Panah <21spanah@gmail.com>
2026-05-18 17:41:16 -0700

    describe what you did

---
+++ scripts/01_load_and_clean.R
@@ -0,0 +1,6 @@
+# Project 1
+
+# How do earnings differ among California workers with no college, some college,
+# and a completed bachelor's degree, and does the education premium vary by 
+# occupation, industry, or demographic group?
+
