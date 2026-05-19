b76876a087f5928a5a0819abe690704485d42cb8
Sina Panah <21spanah@gmail.com>
2026-05-18 17:57:14 -0700

    describe what you did

--- scripts/01_load_and_clean.R
+++ scripts/01_load_and_clean.R
@@ -4,3 +4,16 @@
 # and a completed bachelor's degree, and does the education premium vary by 
 # occupation, industry, or demographic group?
 
+library(tidyverse)
+library(fixest)
+library(modelsummary)
+library(ggplot2)
+library(ipumsr)
+
+setwd("~/ca-education-wage-premium")
+df <- read_csv("data/usa_00001.csv")
+
+glimpse(df)
+summary(df)
+nrow(df)
+names(df)
